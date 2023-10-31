8ccc0a73b772dc467d4fa021fc45ef22e3b4dc94
Ignat Maloukhov <cityofdrawbridges@gmail.com>
2023-10-31 23:56:09 +0300

    Initial commit

---
+++ src/test/java/demo/ignat/springmicroservices/SpringMicroservicesInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.ignat.springmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
